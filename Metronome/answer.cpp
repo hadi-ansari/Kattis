e40561c45408b4f725c4ed2b0413a8dd6567ad81
Hadi Ansari <hadi.ansari161@outlook.com>
2023-02-13 00:12:23 +0100

    Added new easy challenge and solve it in c++ and javascript

---
+++ Metronome/answer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float input;
+    input = 99;
+    float result = input / 4;
+    cout << result;
+    return 0;
+}
---
+++ Metronome/answer.js
@@ -0,0 +1,15 @@
+const readline = require("readline")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+if (!process.stdin.isTTY) {
+    // Input is coming from a file, so don't print the input
+    rl._writeToOutput = () => {}
+}
+
+
+rl.on("line", (input) => {
+    console.log("Input was => ", input)
+    // Your code that uses the input goes here
+})
