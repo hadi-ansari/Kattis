df3dacf887ede282f0ccba17bc005b2c04f18c2d
Hadi Ansari <hadi.ansari161@outlook.com>
2023-02-25 20:17:25 +0100

    Simplified the code for Abandoned_animal

--- Abandoned_animal/answer.py
+++ Abandoned_animal/answer.py
@@ -1,47 +1,35 @@
-def find_path(purchased_items_list, num_stores, stores, path):
+def find_path(purchased_items_list, num_stores, items, path = False):
     for i in range(num_stores):
         while True:
-            if len(purchased_items_list) > 0 and purchased_items_list[0] in stores[i]:
-                purchase = dict()
-                purchase["store"] = i
-                purchase["item"] = purchased_items_list.pop(0)
-                path.append(purchase)
+            if len(purchased_items_list) > 0 and i in items[purchased_items_list[0]]:
+                if not path == False:
+                    purchase = dict()
+                    purchase["store"] = i
+                    purchase["item"] = purchased_items_list.pop(0)
+                    path.append(purchase)
+                else:
+                    purchased_items_list.pop(0)
             else:
                 break
 
     if len(purchased_items_list) > 0:
         return False
     return True
-
-def find_path2(purchased_items_list, num_stores, stores):
-    for i in range(num_stores):
-        while True:
-            if len(purchased_items_list) > 0 and purchased_items_list[0] in stores[i]:
-                purchased_items_list.pop(0)
-            else:
-                break
-
-    if len(purchased_items_list) > 0:
-        return False
-    return True
-
 
 def main():
     num_stores = int(input())
     num_items = int(input())
 
-    stores = {}
+    items = {}
 
     for i in range(num_items):
         line = input().split()
         store_idx = int(line[0])
         item_name = line[1]
-        if not store_idx in stores:
-            stores[store_idx] = [item_name]
+        if not item_name in items:
+            items[item_name] = {store_idx}
         else:
-            items_list = list(stores[store_idx])
-            items_list.append(item_name)
-            stores[store_idx] = items_list
+            items[item_name].add(store_idx)
 
 
     purchased_items = int(input())
@@ -51,19 +39,16 @@ def main():
         purchased_items_list.append(input())
 
     path = []
-    temp_purchased_items_list = list(purchased_items_list)
-    if not find_path(temp_purchased_items_list, num_stores, stores, path):
-        return "impossible"
 
+    temp_purchased_items_list = list(purchased_items_list)
+    if not find_path(temp_purchased_items_list, num_stores, items, path):
+        return "impossible"
     # Finding out if the path is unique?
     for purchase in path:
-        temp_stores = dict(stores)
         temp_purchased_items_list = list(purchased_items_list)
-        temp_list = list(temp_stores[purchase["store"]])
-        temp_list.remove(purchase["item"])
-        temp_stores[purchase["store"]] = temp_list
+        temp_items = {item: set(stores) - {purchase["store"]} for item, stores in items.items()}
 
-        if find_path2(temp_purchased_items_list, num_stores, temp_stores):
+        if find_path(temp_purchased_items_list, num_stores, temp_items):
             return "ambiguous"
        
     return "unique"
